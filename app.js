4b29cdcd84101c55e9d38aa147e28a67071d5e0f
YAcodingroom <n12a18a@gmail.com>
2024-01-03 21:51:10 +0800

    build: use express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 'use strict'
 import express from 'express'
+import { engine } from 'express-handlebars'
 
 const app = express()
 const port = 3000
 
 app.use(express.static('public'))
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
 
 app.get('/', (req, res) => {
-	res.send('Shorten URL web app')
+	res.render('index')
 })
 
 app.post('/shorten', (req, res) => {
